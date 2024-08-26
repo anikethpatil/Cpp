259e68bf03d5e06ab4bb7997da26c20c6b0fd249
D Aniketh Patil <153365406+anikethpatil@users.noreply.github.com>
2024-08-26 15:04:19 +0530

    learn

---
+++ OOPS/Inheritance/Multiple_inheritance.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+public:
+    string name;
+    int age;
+    double cgpa;
+};
+class Cr{
+public:
+    string quality;
+};
+class CollegePresident: public Cr,public Student{
+public:
+    long int vote;
+    void display(){
+        cout<<name<<" "<<age<<" "<<cgpa<<" "<<quality<<" "<<vote<<endl;
+    }
+};
+int main(){
+    CollegePresident s1;
+    s1.name="Anil";
+    s1.age=21;
+    s1.cgpa=8.77;
+    s1.quality="Leader";
+    s1.vote=100000;
+    s1.display();
+    return 0;
+}
